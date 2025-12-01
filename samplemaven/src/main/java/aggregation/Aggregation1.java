47f0d42f51d459b7a3acbb3da54cec04b2d277fa
MERIN MATHEW <MERIN MATHEW@DESKTOP-3N1AQJ3>
2025-12-01 10:53:06 +0530

    01-12-2025

---
+++ samplemaven/src/main/java/aggregation/Aggregation1.java
@@ -0,0 +1,14 @@
+package aggregation;
+
+public class Aggregation1 {
+	String name;
+	int rollno;
+	String address;
+	public Aggregation1(String n,int rollno, String address)
+	{
+		this.name=n;
+		this.rollno=rollno;
+		this.address=address;
+	}
+
+}
